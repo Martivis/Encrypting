ddb308ec24605304a7384e9ca63688f84296bb03
Martivis <vanek-non@rambler.ru>
2023-11-28 23:46:14 +0300

    Implement DTEncryptor

---
+++ DoubleTransposition/DTEncryptor.cs
@@ -0,0 +1,30 @@
+﻿namespace DoubleTransposition;
+
+public static class DTEncryptor
+{
+    public static string Encrypt(string source, Key key1, Key key2)
+    {
+        var columns = key1.Length;
+        var rows = key2.Length;
+        var matrix = new char[key1.Length, key2.Length];
+        
+        for (var i = 0; i < columns; i++)
+        {
+            for (var j = 0; j < rows; j++)
+            {
+                matrix[key1.Sequence.IndexOf(i) , key2.Sequence.IndexOf(j)] = source[i + j];
+            }
+        }
+
+        var charResult = new char[matrix.Length];
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < columns; j++)
+            {
+                charResult[i + j] = matrix[i, j];
+            }
+        }
+
+        return new string(charResult);
+    }
+}
---
+++ DoubleTransposition/Key.cs
@@ -0,0 +1,29 @@
+﻿namespace DoubleTransposition;
+
+public class Key
+{
+    public IList<int> Sequence { get; }
+
+    public int Length => Sequence.Count;
+
+    public Key(string key)
+    {
+        Sequence = ParseKey(key);
+    }
+
+    private IList<int> ParseKey(string key)
+    {
+        return key.Select(c => (int)c - 1).ToList();
+    }
+
+    private bool IsSequenceValid()
+    {
+        var sorted = Sequence.Order().ToList();
+        for (var i = 0; i < Length; i++)
+        {
+            if (sorted[i] != i)
+                return false;
+        }
+        return true;
+    }
+}
