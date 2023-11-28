44f791afa10ef7c3f6cf8f155c527294b44f7b93
Martivis <vanek-non@rambler.ru>
2023-11-29 01:42:42 +0300

    Add DTDecryptorDecryptTests

---
+++ Tests/DoubleTransposition.Tests/DTDecryptorDecryptTests.cs
@@ -0,0 +1,19 @@
+﻿namespace DoubleTransposition.Tests;
+
+public class DTDecryptorDecryptTests
+{
+    [Theory]
+    [InlineData("аенофоее_отпакис_ввшнрийр", "35214", "43251", "шифрование_перестановкой_")]
+    public void EncryptingTest(string source, string sequence1, string sequence2, string expected)
+    {
+        // Arrange
+        var key1 = new Key(sequence1);
+        var key2 = new Key(sequence2);
+        
+        // Act
+        var actual = DTDecryptor.Decrypt(source, key1, key2);
+        
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
